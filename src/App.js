10a9c435fe0700d04e1c3b9689ec8df7c5aab213
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2024-10-19 21:28:08 +0330

    :fire: Delete unneccasary packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import React from 'react'
+import Header from './components/Header'
+import Home from './components/Home'
+import './App.css'
 
 function App() {
   return (
@@ -11,7 +9,7 @@ function App() {
       <Header />
       <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/constants/cars.js
@@ -0,0 +1,8 @@
+const cars = [
+  "Model S",
+  "Model 3",
+  "Model X",
+  "Model Y"
+]
+
+export default cars
